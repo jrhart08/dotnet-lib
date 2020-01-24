1f069ecf1db139220d3d4ef27a460b5f3b0f4647
Joseph Hart <josephrhart@gmail.com>
2020-01-23 21:21:36 -0600

    feat: adding Negate, PickBy, and OmitBy

---
+++ NDash/NDash.OmitBy.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Dynamic;
+
+namespace NDash
+{
+    public static partial class NDashLib
+    {
+        /// <summary>
+        /// (Inverse of PickBy): Returns a new ExpandoObject consisting of the source object's properties which DO NOT match the given predicate.
+        /// </summary>
+        /// <param name="sourceObject">The object to pick properties from.</param>
+        /// <param name="predicate">A predicate to apply to each property on the given object. The second parameter is the name of the property.</param>
+        /// <returns></returns>
+        public static ExpandoObject OmitBy(object sourceObject, Func<object, string, bool> predicate)
+        {
+            return PickBy(sourceObject, Negate(predicate));
+        }
+
+        /// <summary>
+        /// (Inverse of PickBy): Returns a new ExpandoObject consisting of the source object's properties which DO NOT match the given predicate.
+        /// </summary>
+        /// <param name="sourceObject">The object to pick properties from.</param>
+        /// <param name="predicate">A predicate to apply to each property on the given object.</param>
+        /// <returns></returns>
+        public static ExpandoObject OmitBy(object sourceObject, Func<object, bool> predicate)
+        {
+            // return InverseOf(PickBy)(predicate);
+            return PickBy(sourceObject, Negate(predicate));
+        }
+
+        //public static Func<TResult, TFunc> InverseOf<T, TFunc, TResult>(Func<Func<T, bool>, TFunc> predicateOperator)
+        //{
+
+        //    return predicate => predicateOperator(Negate(predicate));
+        //}
+    }
+}
---
+++ NDash/NDash.PickBy.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Reflection;
+
+namespace NDash
+{
+    public static partial class NDashLib
+    {
+        /// <summary>
+        /// Returns a new ExpandoObject consisting of the source object's properties which match the given predicate.
+        /// </summary>
+        /// <param name="sourceObject">The object to pick properties from.</param>
+        /// <param name="predicate">A predicate to apply to each property on the given object. The second parameter is the name of the property.</param>
+        /// <returns></returns>
+        public static ExpandoObject PickBy(object sourceObject, Func<object, string, bool> predicate)
+        {
+            var pickedProperties = sourceObject
+                .GetType()
+                .GetProperties(BindingFlags.Instance | BindingFlags.Public)
+                .Select(prop => Tuple.Create(prop.GetValue(sourceObject), prop.Name))
+                .Where(pair => predicate(pair.Item1, pair.Item2));
+
+            var subObject = new ExpandoObject();
+            var shortcut = subObject as IDictionary<string, object>;
+
+            foreach (var (val, key) in pickedProperties)
+            {
+                shortcut.Add(key, val);
+            }
+
+            return subObject;
+        }
+
+        /// <summary>
+        /// Returns a new ExpandoObject consisting of the source object's properties which match the given predicate.
+        /// </summary>
+        /// <param name="sourceObject">The object to pick properties from.</param>
+        /// <param name="predicate">A predicate to apply to each property on the given object.</param>
+        /// <returns></returns>
+        public static ExpandoObject PickBy(object sourceObject, Func<object, bool> predicate)
+        {
+            // shape this predicate for extended PickBy function
+            Func<object, string, bool> paddedPredicate =
+                (propertyValue, propertyName) => predicate(propertyValue);
+
+            return PickBy(sourceObject, paddedPredicate);
+        }
+    }
+}
