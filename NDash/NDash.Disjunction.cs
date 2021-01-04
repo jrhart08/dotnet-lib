d2d9f3d9be2e716d1967ec1ecefdad861be55661
Joseph Hart <josephrhart@gmail.com>
2021-01-04 16:26:41 -0600

    feat: adding disjunction functions

---
+++ NDash/NDash.Disjunction.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NDash
+{
+    public static partial class NDashLib
+    {
+        // TODO: upgrade to C# 9 for records
+        public class DisjunctionResult<T> : IEnumerable<T>
+        {
+            public IEnumerable<T> Left { get; private set; }
+            public IEnumerable<T> Right { get; private set; }
+
+            public DisjunctionResult(IEnumerable<T> left, IEnumerable<T> right)
+            {
+                Left = left;
+                Right = right;
+            }
+
+            public void Deconstruct(out IEnumerable<T> left, out IEnumerable<T> right)
+            {
+                left = Left;
+                right = Right;
+            }
+        }
+
+        public static DisjunctionResult<T> Disjunction<T>(this IEnumerable<T> leftSource, IEnumerable<T> rightSource)
+        {
+            var uniqueToLeft = leftSource.Except(rightSource);
+            var uniqueToRight = rightSource.Except(leftSource);
+
+            return new DisjunctionResult<T>(uniqueToLeft, uniqueToRight);
+        }
+
+        public static IEnumerable<T> DisjunctiveUnion<T>(this IEnumerable<T> leftSource, IEnumerable<T> rightSource)
+        {
+            var(left, right) = leftSource.Disjunction(rightSource);
+
+            return left.Concat(right);
+        }
+}
+}
